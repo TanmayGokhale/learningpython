153883ee0526a71acb1eebd5af881b5349fa56c7
cmajayg <ajay.g@callme.co.in>
2024-05-01 11:28:37 +0530

    Pandas test
    Pandas test

---
+++ pandas_1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df = pd.read_csv('vgsales.csv')
+print(df)
+print('-----------------------------');
+print(df.shape)
+print('-----------------------------');
+print(df.aggregate)
+
